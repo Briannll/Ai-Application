7b30037c8f869167269396ca3a356db34c83fa93
Brian Lai <150647749+Briannll@users.noreply.github.com>
2024-03-26 19:24:08 -0700

    Added gemini responses

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -4,17 +4,36 @@ import runChat from "../config/gemini";
 export const Context = createContext();
 
 const ContextProvider = (props) => {
-  
-    const [input, setInput] = useState('');
-    
-  
-    const onSent = async (prompt) => {
-    await runChat(prompt);
+  const [input, setInput] = useState("");
+  const [recentPrompt, setRecentPrompt] = useState("");
+  const [prevPrompt, setPrevPrompt] = useState([]);
+  const [showResult, setShowResult] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [resultData, setResultData] = useState("");
+
+  const onSent = async (prompt) => {
+    setResultData("");
+    setLoading(true);
+    setShowResult(true);
+    setRecentPrompt(input);
+    const response = await runChat(input);
+    setResultData(response);
+    setLoading(false);
+    setInput("");
   };
 
-
-
-  const contextValue = {};
+  const contextValue = {
+    prevPrompt,
+    setPrevPrompt,
+    onSent,
+    setRecentPrompt,
+    recentPrompt,
+    showResult,
+    loading,
+    resultData,
+    input,
+    setInput,
+  };
   return (
     <Context.Provider value={contextValue}>{props.children}</Context.Provider>
   );
